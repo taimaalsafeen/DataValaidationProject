52bb9d15543df6cc40b5a708470f9bdae24c1671
LO <LO@DESKTOP-FF7F3TO>
2025-04-05 17:17:31 +0300

    Registration Form

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -30,6 +30,50 @@ namespace DataValaidationProject.Controllers
         {
             return View();
         }
+        // Method for remote validation
+        [AcceptVerbs("GET", "POST")]
+        public IActionResult CheckUsername(string username)
+        {
+            // Simulate checking username against a database
+            var takenUsernames = new List<string> { "admin", "root", "system", "moderator", "user" };
 
+            if (takenUsernames.Contains(username.ToLower()))
+            {
+                return Json($"The username {username} is already taken.");
+            }
+
+            return Json(true);
+
+        }
+        // GET: /Account/Register
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        // POST: /Account/Register
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Register(UserRegistration model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            return RedirectToAction("RegisterationSuccess");
+        }
+
+        // GET: /Account/RegisterationSuccess
+        [HttpGet]
+        public IActionResult RegisterationSuccess()
+        {
+            return View();
+        }
     }
 }
+
+
+
+    
+
