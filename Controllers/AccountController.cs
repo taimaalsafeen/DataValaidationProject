b4c913218c37a1c85447562bfb4533c59e6023ea
LO <LO@DESKTOP-FF7F3TO>
2025-04-05 16:55:43 +0300

    Login Form

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,35 @@
+﻿using DataValidationProject.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DataValaidationProject.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: /Account/Login
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        // POST: /Account/Login
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Login(UserLogin model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            return RedirectToAction("LoginSuccess");
+        }
+
+        // GET: /Account/LoginSuccess
+        [HttpGet]
+        public IActionResult LoginSuccess()
+        {
+            return View();
+        }
+
+    }
+}
