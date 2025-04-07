22ea85bc92be7e9e7ea3a3198df752beb3bcaf30
LO <LO@DESKTOP-FF7F3TO>
2025-04-07 12:56:45 +0300

    HelloPage

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,5 +28,9 @@ namespace DataValaidationProject.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        public IActionResult Hello()
+        {
+            return View();
+        }
     }
 }
